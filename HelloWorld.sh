adf1a2d16b2607b4983280c63c91c27459bab5be
susanthnsk <susanthnsks@gmail.com>
2020-11-01 07:31:08 +0000

    added statemen clone for AUG-release

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,2 +1,3 @@
-".gitconfig" 3L, 57C                                                                                                                                  3,33-40       All
- hello world im susanth
+".gitconfig" 3L, 57C                                                                                                                                  3,33-40       Al
+
+ hello world im susanthnsk
